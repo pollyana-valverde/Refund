9571db5dae80fc64e7d77f1dcc066c1555a8bcbc
Pollyana Valverde <valverdepolly@gmail.com>
2025-10-10 08:39:34 -0300

    Aceitando apenas valor numérico no input

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const amount = document.getElementById("amount");
 const expense = document.getElementById("expense");
 const category = document.getElementById("category");
 
+amount.oninput = () => {
+
+    // Substituindo as letras por nada, aceitando apenas valor numérico
+    let value = amount.value.replace(/\D/g, "")
+
+    // Transforma o valor em centavos (ex: 150/100 = 1.5 que é equivalente a R$ 1,50)
+    value = Number(value) / 100;
+
+    // Atuliza o valor do input
+    amount.value = formarCurrencyBRL(value)
+}
+
