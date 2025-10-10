dccb969f50cf019d386a336a82141baeb6ae45c7
Pollyana Valverde <valverdepolly@gmail.com>
2025-10-10 08:46:59 -0300

    Criando um objeto de nova despesa

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ function formarCurrencyBRL(value) {
     // Retorna o valor formatado
     return value
 }
+
+// Captura o evento de submit do formulário para obter os valores
+form.onsubmit = (e) => {
+    // Previne o comportamento padrão do formulário
+    e.preventDefault()
+
+    // Cria um objeto com os detalhes da nova despesa
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text, //Pega apenas a opção selecionada
+        amount: amount.value,
+        created_at: new Date()
+    }
+
+}
