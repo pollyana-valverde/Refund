f9fbe6206c56ee832e1035ccc4eff031c2ff040a
Pollyana Valverde <valverdepolly@gmail.com>
2025-10-10 08:38:46 -0300

    Selecionando os elementos do formulário

---
+++ script.js
@@ -0,0 +1,6 @@
+// Seleciona os elementos dos formulários
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
