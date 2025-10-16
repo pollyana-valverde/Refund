278cae9bb683c3e5abb0bbcf30651ea8dec5d824
Pollyana Valverde <valverdepolly@gmail.com>
2025-10-16 12:09:06 -0300

    Script principal que agrupa todas as contantes e os módulos

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
 // Importa os módulos necessários
-import { formarCurrencyBRL } from "./formatCurrency.js";
+import { fetchInitialData } from "./initialData.js";
+import { submitData } from "./submitData.js";
+
 import { expenseAdd } from "./addExpense.js";
 import { uptadeTotal } from "./totalUptdate.js";
+import { removeExpense } from "./removeExpense.js";
+
+import { formarCurrencyBRL } from "./formatCurrency.js";
+import { inputFormatCurrency } from "./inputFormatCurrency.js";
 
 // Seleciona os elementos dos formulários
 const form = document.querySelector("form");
@@ -13,52 +19,14 @@ const expensesList = document.querySelector("ul")
 const expenseLength = document.getElementById("expense-length")
 const expensesTotal = document.getElementById("expensesTotal")
 
-// Evento de input para formatar o valor monetário enquanto o usuário digita
-amount.oninput = () => {
+// Chama a função para buscar e exibir os dados iniciais ao carregar a página
+fetchInitialData(form, expense, expensesList, expenseLength, expensesTotal, formarCurrencyBRL, expenseAdd, uptadeTotal);
 
-    // Substituindo as letras por nada, aceitando apenas valor numérico
-    let value = amount.value.replace(/\D/g, "")
+// Configura o evento de submit do formulário para adicionar novas despesas
+submitData(form, amount, category, expense, expensesList, expenseLength, expensesTotal, formarCurrencyBRL, expenseAdd, uptadeTotal );
 
-    // Transforma o valor em centavos (ex: 150/100 = 1.5 que é equivalente a R$ 1,50)
-    value = Number(value) / 100;
+// Configura o evento de clique para remover despesas
+removeExpense(expense, expensesList, expenseLength, expensesTotal, formarCurrencyBRL, uptadeTotal);
 
-    // Atuliza o valor do input
-    amount.value = formarCurrencyBRL(value)
-}
-
-// Captura o evento de submit do formulário para obter os valores
-form.onsubmit = (e) => {
-    // Previne o comportamento padrão do formulário
-    e.preventDefault()
-
-    // Cria um objeto com os detalhes da nova despesa
-    const newExpense = {
-        id: new Date().getTime(),
-        expense: expense.value,
-        category_id: category.value,
-        category_name: category.options[category.selectedIndex].text, //Pega apenas a opção selecionada
-        amount: amount.value,
-        created_at: new Date()
-    }
-
-    // Chama a função para adicionar a nova despesa
-    expenseAdd(newExpense, expensesList, form, expense, expenseLength, expensesTotal, formarCurrencyBRL, uptadeTotal)
-}
-
-// Evento de clique para remover uma despesa
-expensesList.addEventListener("click", function (e) {
-    //Verifica se o elemento clicado é o ícone de remoção
-    if (e.target.classList.contains("remove-icon")) {
-        // Obtém o elemento pai (a despesa) e o remove da lista
-        const item = e.target.parentElement;
-
-        // Remove o item da lista
-        item.remove();
-    }
-
-    // Atualiza os totais após a remoção
-    uptadeTotal(expensesList, expenseLength, expensesTotal, formarCurrencyBRL)
-
-    // Foca no campo de despesa
-    expense.focus()
-})
+// Formata o campo de valor enquanto o usuário digita
+inputFormatCurrency(amount, formarCurrencyBRL);
